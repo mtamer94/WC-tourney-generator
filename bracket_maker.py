fe87b5b9bd0d158b2d02152801d116d0433f1b32
mtamer94 <m.tamer94@gmail.com>
2019-01-25 02:45:46 -0500

    refactoring

---
+++ bracket_maker.py
@@ -0,0 +1,23 @@
+# Second try at this
+
+
+
+While True:
+
+# Gret user and acquire information like player count, random teams etc.
+greet_user() 
+
+# Assign teams if random
+assign_teams()
+
+# Allow them to input teams manually if they choose
+# For each name, this should assign a team
+# Make this a class so we can count wins etc? though
+select_teams()
+
+# Export bracket to Google sheets or excel
+export_bracket()
+
+# Email bracket to as many people who want
+
+
